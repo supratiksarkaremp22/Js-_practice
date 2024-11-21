ed05a994d93328e24f35b3bd597bd646db8e3bed
Supratik Sarkar <87625070+supratiktechandanalytics@users.noreply.github.com>
2024-11-21 17:10:12 +0530

    Delete basics directory

--- basics/day1.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-alert(3+3)
-/*there is separate syntax available in node to
-use alert*/
--- basics/day1a.js
+++
@@ -1,6 +0,0 @@
-
-console.log(3+3); console.log("Supratik")
-//unreadable code
-console.log(3+3)
-console.log("Supratik")
-//readable code 
